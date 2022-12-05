2f30182db86691ebf16d889c5318643d1b638472
dhanasekharreddyalimili <36956267+dhanasekharreddyalimili@users.noreply.github.com>
2022-12-05 06:27:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Welcome to AKS which was intraduced by Microsoft....Welcome to python application.... (Python)! (up %s)\n" % elapsed()
+    return "Welcome to Microsoft Azure Cloud Computing...Welcome to 2023.... (Python)! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
